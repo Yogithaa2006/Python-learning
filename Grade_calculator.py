084b5c81a529b297ee49e03c77758e1eef7fa31b
Yogithaa S <yogithaa2006@gmail.com>
2026-06-15 23:29:20 +0530

    Grade_calculator

---
+++ Grade_calculator.py
@@ -0,0 +1,9 @@
+mark=int(input("Enter a student mark:"))
+if(mark>=90):
+    print("Grade A")
+elif(mark>=75 and mark<=89):
+    print("Grade B")
+elif(mark>=50 and mark<=74):
+    print("Grade C")
+elif(mark<=50):
+    print("Grade F")
